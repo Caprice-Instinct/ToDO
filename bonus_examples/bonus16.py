47d841b46fd9bd0ef353ab329b684a361f8e3df4
Caprice-Instinct <linetwangui04@gmail.com>
2024-10-13 23:31:09 +0300

    Implement complete and exit buttons

--- bonus_examples/bonus16.py
+++ bonus_examples/bonus16.py
@@ -1,19 +1,29 @@
 import FreeSimpleGUI as sg
+from zip_creator import make_archive
 
 label1 = sg.Text("Select files for compression: ")
 input1 = sg.Input()
-choose_btn1 = sg.FilesBrowse("Choose")
+choose_btn1 = sg.FilesBrowse("Choose", key="files")
 
 label2 = sg.Text("Choose Destination folder: ")
 input2 = sg.Input()
-choose_btn2 = sg.FolderBrowse("Choose")
+choose_btn2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_btn = sg.Button("Compress")
+output_label = sg.Text(key="result", text_color="red")
 
 window = sg.Window("File Compressor",
                    layout=[[label1, input1, choose_btn1],
                            [label2, input2, choose_btn2],
-                           [compress_btn]])
+                           [compress_btn, output_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    filepaths = values['files'].split(";")
+    folder = values['folder']
+    make_archive(filepaths, folder)
+    window['result'].update(value="Compression completed successfully!")
+
 window.close()
--- coding experiments/code-ex4.py
+++ coding experiments/code-ex4.py
@@ -1,18 +1,25 @@
 import FreeSimpleGUI as sg
+from bonus_examples.converter14 import convert
 
 label1 = sg.Text("Enter feet:")
-input1 = sg.Input()
+input1 = sg.Input(key="feet")
 
 label2 = sg.Text("Enter inches:")
-input2 = sg.Input()
+input2 = sg.Input(key="inches")
 
 convert_btn = sg.Button("Convert")
+output_label = sg.Text(key="output")
 
 window = sg.Window("Convertor",
                    layout=[[label1, input1],
                            [label2, input2],
-                           [convert_btn]])
+                           [convert_btn, output_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    feet = float(values['feet'])
+    inches = float(values['inches'])
+    meters = convert(feet, inches)
+    window['output'].update(value=f"You are {meters}m")
 print("Bye")
 window.close()
